34b719fcd8f5e636fec68f9e04b0983171af2010
makvool <makvool@freemail.gr>
2011-10-01 02:37:56 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dentist::Application.config.secret_token = '1a41f7cf916dddc9cd698b9a5e5b84656e1a4738c9bab340fd1ccd3bd66c44e1f29b46c4d034578a12ea3cf8e1c65f658e130a5e32544ab98b79ae09c3b06433'
