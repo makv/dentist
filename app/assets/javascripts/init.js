ed751d051d2fdc223d216205995ef0527f781a90
makvool <makvool@freemail.gr>
2011-10-12 14:06:28 +0300

    accordion fixes

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -4,23 +4,6 @@ $(document).ready(function() {
 	});
 	
 $('ul.sf-menu').superfish();				
-$(".accordion").each(function(){
-
-   $(this).find('.acc_container').hide();	//Hide/close all containers
-   $(this).find('.acc_trigger:first').addClass('active').next().show();	 //Add "active" class to first trigger, then show/open the immediate next container
-
-	//On Click
-    
-    $(this).find('.acc_trigger').click(function(){
-		if( $(this).next().is(':hidden') ) { //If immediate next container is closed...
-        $(this).parent().find('.acc_trigger').removeClass('active').next().slideUp(); //Remove all .acc_trigger classes and slide up the immediate next container
-        $(this).toggleClass('active').next().slideDown(); //Add .acc_trigger class to clicked trigger and slide down the immediate next container
-	}
-    	$(this).children("a:first").blur();	//remove the dotted box around the anchor
-	return false; //Prevent the browser jump to the link anchor
-	});
-});
-
 
 // setup ul.tabs to work as tabs for each div directly under div.panes       
 // $("ul.tabs").tabs("div.panes > div", { history: true });
