5f58f351486892176f5448bec8152b58b451e1c4
makvool <makvool@freemail.gr>
2011-10-14 22:04:14 +0300

    slideshow

--- config/application.rb
+++ config/application.rb
@@ -40,7 +40,7 @@ module Dentist
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    config.assets.enabled = true
+    config.assets.enabled = false
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
     config.assets.initialize_on_precompile = false
     # Version of your assets, change this if you want to expire all your assets
