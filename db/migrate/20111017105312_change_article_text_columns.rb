0648bba462e3cbf5d356a1d6b88616fa63d5a252
makvool <makvool@freemail.gr>
2011-10-17 20:17:33 +0300

    admin

--- db/migrate/20111017105312_change_article_text_columns.rb
+++
@@ -1,8 +0,0 @@
-class ChangeArticleTextColumns < ActiveRecord::Migration
-  def change
-    change_column :articles, :description, :text
-    change_column :articles, :method, :text
-    drop_table :periodontology_articles
-    drop_table :implants_articles
-  end
-end
--- db/migrate/20111017141938_change_article_text_columns_length.rb
+++
@@ -1,6 +0,0 @@
-class ChangeArticleTextColumnsLength < ActiveRecord::Migration
-  def change
-    change_column :articles, :description, :text, :limit => 1024
-    change_column :articles, :method, :text, :limit => 1024
-  end
-end
